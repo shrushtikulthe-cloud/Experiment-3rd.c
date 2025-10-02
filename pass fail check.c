ce5b55427e907a39f0afd89d549319a93b15cbdd
shrushtikulthe-cloud <shrushtikulthe@gmail.com>
2025-10-02 23:14:54 +0530

    Create pass fail check.c

---
+++ pass fail check.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int marks;
+    printf("Enter Marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 40) printf("Pass\n");
+    else printf("Fail\n");
+
+    return 0;
+}
